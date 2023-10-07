e23578d4d472f94690cfcf9d476174976fb73865
can101 <cancelikdev@outlook.com>
2023-10-07 17:32:44 +0300

    fix: removed helloworld component import

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/app">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
