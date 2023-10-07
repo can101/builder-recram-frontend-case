ecbecf48871b28f321fea22096daaf3ca9b77a70
can101 <cancelikdev@outlook.com>
2023-10-07 17:23:10 +0300

    style: modifeied router and view

---
+++ src/views/AppView.vue
@@ -0,0 +1,30 @@
+<template>
+  <HeaderComponent />
+  <div class="h-[calc(100%-56px)] flex">
+    <SidebarComponent />
+    <div class="w-full">
+      <FormPreview />
+      <PagesDraggableAreaBox />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import FormPreview from '@/components/FormPreview.vue'
+import SidebarComponent from '@/components/Sidebar.vue'
+import HeaderComponent from '@/components/Header.vue'
+import PagesDraggableAreaBox from '@/components/PagesDraggableAreaBox.vue'
+</script>
+
+<style lang="scss">
+.width {
+  &__size {
+    &_wide {
+      @apply w-3/6 justify-center;
+    }
+    &_full {
+      @apply w-full justify-center;
+    }
+  }
+}
+</style>
